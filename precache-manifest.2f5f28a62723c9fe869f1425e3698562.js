ae71c192ace07522ed8eb06892d2813af7fdfd97
Daniel Zaharia <daniel.zaharia.corona@gmail.com>
2019-04-02 20:11:15 +0200

    Updates

---
+++ precache-manifest.2f5f28a62723c9fe869f1425e3698562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d43fcecf4928b0c254b",
+    "url": "/Contact-Manager/static/js/runtime~main.e499c403.js"
+  },
+  {
+    "revision": "ebaba901e32a34676d5e",
+    "url": "/Contact-Manager/static/js/main.2ab77cf9.chunk.js"
+  },
+  {
+    "revision": "1328e38f58a3da3330a5",
+    "url": "/Contact-Manager/static/js/2.20d82ee2.chunk.js"
+  },
+  {
+    "revision": "ebaba901e32a34676d5e",
+    "url": "/Contact-Manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1328e38f58a3da3330a5",
+    "url": "/Contact-Manager/static/css/2.24299406.chunk.css"
+  },
+  {
+    "revision": "290209e54305b39dd43aec59ec07c91d",
+    "url": "/Contact-Manager/index.html"
+  }
+];
